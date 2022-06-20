bdc65ff7ac88cd106993020e438e4fe72a1233fc
Leonardo Alves <leonardo.alves@eritel.com.br>
2022-06-20 13:39:05 -0300

    Add project files.

---
+++ Longest Substring Without repeating Characters/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Solution Solution = new Solution();
+Console.WriteLine(Solution.LengthOfLongestSubstring("pwwkew"));
---
+++ Longest Substring Without repeating Characters/Solution.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        string text = "";
+        char[] characters = s.ToCharArray();
+        int count = 0;
+        int result = 0;
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (!text.Contains(characters[i]))
+            {
+                text += characters[i];
+            }
+            else
+            {
+                count = text.Length;
+                if (count > result)
+                {
+
+                    result = count;
+                }
+
+                text = "";
+                text += characters[i];
+            }
+
+
+        }
+
+        return result;
+    }
+}
